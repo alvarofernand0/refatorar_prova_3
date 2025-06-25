5bc7c1d8977b2d80887797e304de48a911e0ab23
alvarofernand0 <alvarofernando1324@gmail.com>
2025-06-25 14:34:46 -0300

    Refazendo o conversor de moedas

--- ConversorDeMoedas-JP/Moeda.cs
+++ ConversorDeMoedas-JP/Moeda.cs
@@ -1,5 +1,3 @@
-using System.Text;
-
 namespace ConversorDeMoedas
 {
     public class Moeda
--- ConversorDeMoedas-JP/Program.cs
+++ ConversorDeMoedas-JP/Program.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using ConversorDeMoedas;
 
 List<Moeda> moedas = new List<Moeda>()
 {
-    new Moeda() { Nome = "Real Brasileiro", Cotacao = 1.00m },
-    new Moeda() { Nome = "Dólar", Cotacao = 4.50m },
-    new Moeda() { Nome = "Euro", Cotacao = 6.20m },
-    new Moeda() { Nome = "Iene", Cotacao = 0.052m },
-    new Moeda() { Nome = "Libra Esterlina", Cotacao = 6.95m }
+    new Moeda() {Nome = "Dolar($)", Cotacao = 4.50m},
+    new Moeda() {Nome = "Euro(€)", Cotacao = 5.50m},
+    new Moeda() {Nome = "Iene(¥)", Cotacao = 0.567m},
+    new Moeda() {Nome = "Real Brasileiro(R$)", Cotacao = 1.00m},
+    new Moeda() {Nome = "Peseta(Pts)", Cotacao = 2.35m}
 };
 
-// Ponto de entrada da aplicação
 IniciarAplicacao();
 
 void IniciarAplicacao()
@@ -20,129 +18,125 @@ void IniciarAplicacao()
     while (sair != true)
     {
         Console.Clear();
-        Console.WriteLine("==== Conversor de Moedas ====\n");
+        Console.WriteLine("=== MENU DE MOEDAS ===\n");
         Console.WriteLine("1 - Listar Moedas");
-        Console.WriteLine("2 - Adicionar Moeda");
+        Console.WriteLine("2 - Adicionar Moedas");
         Console.WriteLine("3 - Converter Moedas");
-        Console.WriteLine("4 - SAIR");
-        Console.Write("\nDigite uma das opções: ");
+        Console.WriteLine("4 - SAIR\n");
+        Console.WriteLine("Digite uma das opções...");
 
         int.TryParse(Console.ReadLine(), out int opcao);
-
         switch (opcao)
         {
             case 1:
-                //Console.Clear();
                 ListarMoedas();
                 break;
             case 2:
-                //Console.Clear();
-                AdicionarMoeda();
+                AdicionarMoedas();
                 break;
             case 3:
-                //Console.Clear();
-                ConverterMoeda();
+                ConverterMoedas();
                 break;
             case 4:
-                //Console.Clear();
-                Console.WriteLine("Saindo da aplicação...");
                 sair = true;
+                Console.Clear();
+                Console.WriteLine("Saindo da Aplicação...");
                 break;
             default:
-                Console.WriteLine("\nNão é uma opção válida!");
-                Console.Write("\nDigite qualquer tecla para voltar ao menu: ");
+                Console.WriteLine("Opção inválida!\n");
+                Console.WriteLine("Digite qualquer tecla para voltar ao menu...");
                 break;
         }
-        Console.ReadKey(true);
+        Console.ReadKey();
     }
 }
 
 void ListarMoedas()
 {
-    Console.WriteLine("==== Menu de Listagem ====\n");
+    Console.Clear();
+    Console.WriteLine("=== MENU DE LISTAGEM ===\n");
+
     for (int i = 0; i < moedas.Count; i++)
     {
-        if (moedas.Count == 0)
+        Console.WriteLine("------------------\n");
+
+        Console.WriteLine($"{i + 1}- {moedas[i].Nome} {moedas[i].Cotacao}\n");
+    }
+    Console.WriteLine("------------------\n");
+    Console.WriteLine("Digite qualquer tecla para retornar ao menu...");
+}
+
+void AdicionarMoedas()
+{
+    bool moedaOk = false;
+    while (moedaOk != true)
+    {
+        Console.Clear();
+        Console.WriteLine("=== MENU DE ADIÇÃO DE MOEDAS ===\n");
+
+        Console.Write("Digite o nome da nova Moeda: ");
+        string novoNome = Console.ReadLine();
+
+        Console.WriteLine("Digite uma Cotação para a Moeda");
+        if (!decimal.TryParse(Console.ReadLine(), out decimal novaCotacao) && novaCotacao == 0)
         {
-            Console.WriteLine("A lista está vazia...");
+            Console.WriteLine("Valor inválido!");
+            Console.ReadKey();
         }
         else
         {
-            Console.WriteLine("-------------------\n");
-            Console.WriteLine($"- Moeda: {moedas[i].Nome}");
-            Console.WriteLine($"- Cotação: {moedas[i].Cotacao}");
+            moedas.Add(new Moeda() { Nome = novoNome, Cotacao = novaCotacao });
+            Console.Clear();
+            moedaOk = true;
+            Console.WriteLine("Moeda adicionada com sucesso!\n Digite qualquer tecla para retornar ao Menu...");
+            break;
         }
-        Console.WriteLine("");
-    }
-    Console.WriteLine("-------------------\n");
-}
-
-void AdicionarMoeda()
-{
-    Console.WriteLine("==== Menu de Adição ====\n");
-
-    Console.Write("Digite o nome da moeda: ");
-    string nome = Console.ReadLine();
-    Console.Write("Digite a cotação: ");
-
-    if (!decimal.TryParse(Console.ReadLine(), out decimal cotacao))
-    {
-        Console.WriteLine("Valor incorreto!");
-    }
-    else
-    {
-        moedas.Add(new Moeda() { Nome = nome, Cotacao = cotacao });
-        Console.WriteLine("Moeda adicionada com sucesso!");
     }
 }
 
-void ConverterMoeda()
+void ConverterMoedas()
 {
-    Console.WriteLine("==== Menu de Conversão ====\n");
-
-    for (int i = 0; i < moedas.Count; i++)
+    bool moedaOk = false;
+    while (moedaOk != true)
     {
-        Console.WriteLine($"{i + 1}- {moedas[i].Nome} {moedas[i].Cotacao}");
-    }
+        Console.Clear();
+        
+        for (int i = 0; i < moedas.Count; i++)
+        {
+            Console.WriteLine($"{i}- {moedas[i].Nome} {moedas[i].Cotacao}\n");
+        }
 
-    bool conversao = true;
-    while (conversao != false)
-    {
-        Console.Write("\nDigite o n° da moeda inicial: ");
-        if (int.TryParse(Console.ReadLine(), out int inicio) && inicio < moedas.Count)
+        Console.Write("Digite o Nº da opção da moeda Inicial: ");
+        if (int.TryParse(Console.ReadLine(), out int inicial) && inicial < moedas.Count && inicial >= 0)
         {
             Console.WriteLine("");
         }
         else
         {
-            Console.Write("Opção incorreta! Digite novamente: ");
+            Console.WriteLine("Opção inválida!\nDigite qualquer tecla para tentar novamente...");
         }
 
-        Console.Write("\nDigite o n° da moeda final: ");
-        if (int.TryParse(Console.ReadLine(), out int final) && final >= 0 &&  final < moedas.Count)
+        Console.Write("Digite o Nº da opção da moeda Final: ");
+        if (int.TryParse(Console.ReadLine(), out int final) && final < moedas.Count && final >= 0)
         {
             Console.WriteLine("");
         }
         else
         {
-            Console.Write("Opção incorreta! Digite novamente: ");
+            Console.WriteLine("Opção inválida!\nDigite qualquer tecla para tentar novamente...");
         }
 
-        Console.Write("\nDigite o valor para conversão: ");
-        if (decimal.TryParse(Console.ReadLine(), out decimal valor))
+        Console.Write("Digite o valor que deseja converter: ");
+        if (decimal.TryParse(Console.ReadLine(), out decimal valor) &&  valor == 0)
         {
-            decimal valorConvertido = (valor * moedas[inicio].Cotacao) / moedas[final].Cotacao;
-            Console.WriteLine($"\nO valor convertido é: {valorConvertido:f2} {moedas[final -1].Nome}");
-            conversao = false;
+            Console.WriteLine("Valor inválido!");
         }
         else
         {
-            Console.Write("Valor inválido! Digite novamente: ");
+            decimal valorConvertido = (valor * moedas[inicial].Cotacao) / moedas[final].Cotacao;
+            Console.WriteLine($"O valor convertido é {valorConvertido:f3} {moedas[final].Nome}");
+            moedaOk = true;
+            break;
         }
     }
 }
-
-// Desafio extra:
-// TODO: Permitir converter qualquer moeda para qualquer outra (incluindo Real).
-// TODO: O menu deve ser recursivo, só finalizando quando o usuário escolher "Sair".
-// TODO: Implementar versionamento com Git e subir o projeto no GitHub.
